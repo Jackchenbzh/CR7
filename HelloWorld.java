4928d2e63b56da423919a0b0b9fad6a58c4efff7
chenbzh <1669350948@qq.com>
2016-07-21 15:06:51 +0800

    init commit

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+package com.imooc;
+
+
+public class HelloWorld{
+    public static void main(String[] args) {
+		String number1="ÎÒ°®Ä½¿ÎÍø";
+        String number2="www.imooc.com";
+        System.out.println(number1);
+		System.out.println(number2);
+	}
+}
