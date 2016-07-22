d798cc27fc171c813883c0bcbc926416cbe27add
chenbzh <1669350948@qq.com>
2016-07-22 20:49:18 +0800

    构造方法以及封装

--- TelPhone.java
+++ TelPhone.java
@@ -1,32 +1,53 @@
-package chenbzh.second;
+package chenbzh;
 
 public class TelPhone {
+	//属性,成员变量,封装
 	private double screen;
 	private double cpu;
 	private double men;
-	
-	public void sendMessage(){
-		System.out.println("可以发送短信");
-	}
-	
-	public double getScreen() {
+	//调用
+	public double getScreen(){
 		return screen;
 	}
-	public void setScreen(double screen) {
-		this.screen = screen;
+	
+	public void setScreen(double newScreen){
+		screen = newScreen;
 	}
-	public double getCpu() {
+	
+	public double getCpu(){
 		return cpu;
 	}
-	public void setCpu(double cpu) {
-		this.cpu = cpu;
+	
+	public void setCpu(double newCpu){
+		cpu = newCpu;
 	}
-	public double getMen() {
+	
+	public double getMen(){
 		return men;
 	}
 	
-	public void setMen(double men){
-		this.men = men;
+	public void setMen(double newMen){
+		men = newMen;
 	}
-	
+	//方法，局部变量
+	void call(){
+		System.out.println("具有打电话的功能");		
+	}
+	void text(){
+		System.out.println("screen:"+screen+"cpu:"+cpu+"men:"+men+"具有发短信的功能");
+	}
+	//无参构造方法
+	public TelPhone(){
+		screen = 5.2;
+		cpu = 4.0;
+		men = 3.5;
+		System.out.println("无参构造方法执行了，并设定了默认值");
+	}
+	//有参构造方法
+	public TelPhone(double newscreen,double newcpu,double newmen){
+		screen = newscreen;
+		cpu = newcpu;
+		men = newmen;
+		System.out.println("有参构造方法执行了");
+	}
 }
